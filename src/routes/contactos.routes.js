734a37a286770f46a3827fc1c6bfb283b8ec1494
VisualPrimecode <claudiorodriguez7778@gmail.com>
2024-11-11 04:17:27 -0300

    ruta por defecto

--- src/routes/contactos.routes.js
+++ src/routes/contactos.routes.js
@@ -2,6 +2,8 @@ import { Router } from 'express'
 import {actResp, crearResp, delResp, getResp, obtResp} from '../controllers/contactos.controller.js'
 const router = Router()
 
+    router.get('/',getResp);
+
     router.get("/respuestas", getResp); 
 
     router.get("/respuestas/:id", obtResp);
