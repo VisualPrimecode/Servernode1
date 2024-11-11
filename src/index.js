c5024567aa8715e75d2ad0edbc5482e8b991cc4c
VisualPrimecode <claudiorodriguez7778@gmail.com>
2024-11-11 01:42:16 -0300

    detalle

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const PORT = 3000;
 const HOST = '0.0.0.0'; // Asegúrate de que sea 0.0.0.0
+import app from './app.js'
+
 
 app.listen(PORT, HOST, () => {
     console.log(`Servidor escuchando en http://${HOST}:${PORT}`);
